e53e85e50ba36045ee74b91780508b09524dcc09
Vishnu Prasad <vishnu.msvp@gmail.com>
2017-11-24 23:23:48 +0530

    first Commita

---
+++ API/index.js
@@ -0,0 +1,60 @@
+//requiring NPM modeles
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var app = express();
+
+db.on('error', console.error);
+
+//requiring local modeles
+var configs = require('./config');
+var routes = require('./routes/routes');
+var userModel = require('./models/users');
+var helperFunctions = require('./helpers/helperFunctions');
+
+
+// Uncomment the following lines to start logging requests to consoles.
+// app.use(morgan('combined'));
+// parse application/x-www-form-urlencoded.
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json.
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:'+configs.applicationPort);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+//connedting to mongoDB
+mongoose.connect('mongodb://'+configs.dbHost+'/'+configs.dbName);
+//populating data if DB is not already populated.
+helperFunctions.populateDb();
+
+//Initilizing routes.
+routes(app);
+
+// serve video files.
+app.use('/videos',express.static('videos'));
+// serve client side code.
+app.use('/',express.static('client'));
+
+//Finally starting the listener
+app.listen(configs.applicationPort, function () {
+  console.log('Example app listening on port '+configs.applicationPort+'!');
+});
